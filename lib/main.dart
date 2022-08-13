b6cc87a8c0db09634c4c8de1e7691122f1e1c61b
EdVasquezM <emvasquezm@gmail.com>
2022-08-12 21:20:58 -0500

    funcion de lista infinita y contador de clicks en una sola app

--- lib/main.dart
+++ lib/main.dart
@@ -21,13 +21,36 @@ class MyApp extends StatelessWidget {
           title: const Text('Welcome to the testing app: this is de AppBar'),
         ),
         body: const Center(
-          child: RandomWords(),
-          //child: Text('Hello World! this is the center of the body'),
+          child: DoubleScreen(),
         )
       ),
     );
   }
 }
+
+class DoubleScreen extends StatefulWidget {
+  const DoubleScreen({Key? key}) : super(key: key);
+  @override
+  State<DoubleScreen> createState() => _DoubleScreenState();
+}
+
+class _DoubleScreenState extends State<DoubleScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+      children: const <Widget>[
+        Expanded(
+          child: Center(child: MyHomePage())
+        ),
+        Expanded(
+          child: Center(child: RandomWords())
+        ),
+      ]
+    );
+  }
+}
+
 
 class RandomWords extends StatefulWidget {
   const RandomWords({Key? key}) : super(key: key);
@@ -40,9 +63,6 @@ class _RandomWordsState extends State<RandomWords> {
   final _biggerFont = const TextStyle(fontSize: 18);
   @override
   Widget build(BuildContext context) {
-    //var rNum = Random();
-    //int intRNum = rNum.nextInt(100);
-    //return Text('Try: $intRNum');
     return ListView.builder(
       padding: const EdgeInsets.all(16.0),
       itemBuilder: /*1*/ (context, i) {
@@ -63,61 +83,27 @@ class _RandomWordsState extends State<RandomWords> {
 }
 
 
-//class MyHomePage extends StatefulWidget {
-  //const MyHomePage({Key? key, required this.title}) : super(key: key);
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({Key? key}) : super(key: key);
 
-  //final String title;
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
 
-  //@override
-  //State<MyHomePage> createState() => _MyHomePageState();
-//}
+class _MyHomePageState extends State<MyHomePage> {
+  int _counter = 0;
 
-//class _MyHomePageState extends State<MyHomePage> {
-  //int _counter = 0;
+  void _incrementCounter() {
+    setState(() {
+      _counter++;
+    });
+  }
 
-  //void _incrementCounter() {
-    //setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      //_counter++;
-    //});
-  //}
-
-/*  @override
+  @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
-      appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
       body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
         child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
             const Text(
@@ -134,7 +120,7 @@ class _RandomWordsState extends State<RandomWords> {
         onPressed: _incrementCounter,
         tooltip: 'Increment',
         child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+      ),
     );
   }
-}*/
+}
